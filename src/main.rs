a09757f6b174cd9829359cb985aa6fa4a5bce284
0xMRTT <0xMRTT@tuta.io>
2022-06-17 13:42:53 +0200

    Using tera in rs

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,23 @@
-use actix_web::{get, web, App, HttpServer, Responder};
+use actix_web::{HttpServer, App, web, HttpResponse, Responder};
+use tera::{Tera, Context};
 
-#[get("/hello/{name}")]
-async fn greet(name: web::Path<String>) -> impl Responder {
-    format!("Hello {name}!")
+async fn index(tera: web::Data<Tera>) -> impl Responder {
+    let mut data = Context::new();
+    data.insert("title", "Marketplace");
+
+    let rendered = tera.render("index.html", &data).unwrap();
+    HttpResponse::Ok().body(rendered)
 }
 
-#[actix_web::main] 
+#[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
+        let tera = Tera::new("templates/**/*").unwrap();
         App::new()
-            .route("/hello", web::get().to(|| async { "Hello World!" }))
-            .service(greet)
+            .app_data(tera)
+            .route("/", web::get().to(index))
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind("127.0.0.1:8000")?
     .run()
     .await
 }
